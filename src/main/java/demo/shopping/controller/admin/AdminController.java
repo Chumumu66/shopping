4b7c1342cd31a4668d24dd57aff351ff590b8d5a
chumumu <1516996093@qq.com>
2023-03-27 16:33:46 +0800

    Error处理

--- src/main/java/demo/shopping/controller/admin/AdminController.java
+++ src/main/java/demo/shopping/controller/admin/AdminController.java
@@ -1,29 +1,41 @@
 package demo.shopping.controller.admin;
 import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import demo.shopping.po.Auser;
 import demo.shopping.service.admin.AdminService;
+import org.springframework.web.servlet.ModelAndView;
+
 @Controller
 public class AdminController {
 	@Autowired
 	private AdminService adminService;
+
 	@RequestMapping("/admin")
 	public String toLogin(@ModelAttribute Auser auser) {
 		return "admin/login";
 	}
+
 	@RequestMapping("/admin/login")
-	public String login(@ModelAttribute Auser auser, Model model, HttpSession session) {
+	public String login(@Valid @ModelAttribute Auser auser, Model model, HttpSession session) {
 		return adminService.login(auser, model, session);
 	}
+
 	@RequestMapping("/exit")
 	public String exit(@ModelAttribute Auser auser,HttpSession session) {
 		session.invalidate();
 		return "admin/login";
+	}
+
+	@ExceptionHandler
+	public ModelAndView handleLoginError(Exception ex){
+		return new ModelAndView("error/error");
 	}
 }
