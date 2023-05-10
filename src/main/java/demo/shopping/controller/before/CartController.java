eae5e953e8f7ae6b7944fd6625eea94a342d9fdf
ASUS <2857432280@qq.com>
2023-05-10 16:11:32 +0800

    before下所有页面第二次小修改

--- src/main/java/demo/shopping/controller/before/CartController.java
+++ src/main/java/demo/shopping/controller/before/CartController.java
@@ -75,6 +75,7 @@ public class CartController extends BaseBeforeController{
 	@RequestMapping("/deleteAgoods")
 	public String deleteAgoods(Integer id, HttpSession session, Model model) {
 		logger.log(Level.INFO,"删除对应商品");
+		//System.out.println(111);
 		int flag = cartService.deleteAgoods(id, MyUtil.getUserId(session));
 		if(flag == 0){
 			model.addAttribute("msg", "删除失败！");
